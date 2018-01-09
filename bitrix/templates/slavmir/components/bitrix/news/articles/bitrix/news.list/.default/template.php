e9d8c986ca56ef2529ba9040b3291f081da26826
adoletskiy <adoletsky@indigos.ru>
2018-01-09 19:51:41 +0300

    article page list: add tab

--- bitrix/templates/slavmir/components/bitrix/news/articles/bitrix/news.list/.default/template.php
+++ bitrix/templates/slavmir/components/bitrix/news/articles/bitrix/news.list/.default/template.php
@@ -29,13 +29,53 @@ $this->setFrameMode(true);
 <?}else{?>
 <div class="tabs" id="new_art_tabs">
 	<ul>
-		<li class="active" data-id="all">Все статьи</li>
-		<?/*?><li data-id="2">Все</li><?*/?>
+        <li class="active" data-id="new">Свежие статьи</li>
+        <li data-id="all">Все статьи</li>
 	</ul>
 </div>
 <?}?>
-
-<div class="tab_container new_articles_list active" data-attr="all" data-id="new_art_tabs">
+    <div class="tab_container new_articles_list active" data-attr="new" data-id="new_art_tabs">
+        <?
+        $count=5;
+        foreach( $arResult["ITEMS"] as $arItem ){
+            if(!$count) break;
+            $count--;
+            $sectionID = $arItem["IBLOCK_SECTION_ID"];
+            $section = '';
+            if( isset( $arResult["SECTIONS"][$sectionID] ) ){
+                $section = $arResult["SECTIONS"][$sectionID]["NAME"];
+            }
+            ?>
+            <div class="new_article">
+                <div class="new_article_text">
+                    <?if( $section ){?><p class="new_art_type"><?=$section?></p><?}?>
+                    <?/*?><h3><?=$arItem["NAME"]?></h3>
+				<article><?=$arItem["PREVIEW_TEXT"]?> <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="read_art">Читать</a>
+				</article>
+				<?*/?>
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>">
+                        <span class="article_name"><?=$arItem["NAME"]?> <?if( $arItem["PROPERTIES"]["IS_NO_AUTH"]["VALUE_XML_ID"]!="Y" ){?><span class="subs_read_only"></span><?}?></span>
+                        <span class="article_text"><?=$arItem["PREVIEW_TEXT"]?> <span class="read_art">Читать</span></span>
+                    </a>
+                    <?if( $arItem["PROPERTIES"]["AUTHOR"]["VALUE"] ){
+                        $authorID = $arItem["PROPERTIES"]["AUTHOR"]["VALUE"];
+                        $author = $arResult["AUTHORS"][$authorID];
+                        ?>
+                        <p class="new_art_auth">Автор: <a href=""><?=$author["NAME"]?></a></p>
+                    <?}?>
+                </div>
+                <?
+                $image = GetConfig( "default_image" );
+                if( isset($arItem["PREVIEW_PICTURE"]["SRC"]) && $arItem["PREVIEW_PICTURE"]["SRC"] ) $image = MakeImage( $arItem["PREVIEW_PICTURE"]["SRC"], array("w"=>171,"h"=>171,"zc"=>1) );
+                ?>
+                <div class="new_article_img">
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img src="<?=$image?>" alt="new_art_img.png"></a>
+                </div>
+                <div class="clear"></div>
+            </div><!-- new_article -->
+        <?}?>
+    </div>
+<div class="tab_container new_articles_list" data-attr="all" data-id="new_art_tabs">
 	<div class="ajax-list">
 		<?if( !count( $arResult["ITEMS"] ) ){?>
 		В данном разделе нет статей
