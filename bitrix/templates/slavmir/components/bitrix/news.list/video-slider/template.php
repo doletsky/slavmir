97d97ed1c76a2d37317d699e0ffee0d33ec34e30
adoletskiy <adoletsky@indigos.ru>
2018-01-15 19:42:22 +0300

    debug slider video page

--- bitrix/templates/slavmir/components/bitrix/news.list/video-slider/template.php
+++ bitrix/templates/slavmir/components/bitrix/news.list/video-slider/template.php
@@ -29,9 +29,9 @@
 				$artist = $arResult["ARTISTS"][$artistID]["NAME"];
 			}
 			?>
-			<div class="daily_video_item pl-video-play <?if($needBlock){?>block<?}?>" data-name="<?=$arItem["NAME"]?>" data-artist="<?=$artist?>" data-picture="<?=$image?>" data-url="<?=$url?>">
+			<div class="daily_video_item">
 				<div class="item_img" style="background-image: url(<?=$image?>);"></div>
-				<div class="play_video_btn"><img src="<?=SITE_TEMPLATE_PATH?>/images/play_btn_large.png" alt="<?=$arItem["NAME"]?>"></div>
+				<div class="play_video_btn pl-video-play <?if($needBlock){?>block<?}?>" data-name="<?=$arItem["NAME"]?>" data-artist="<?=$artist?>" data-picture="<?=$image?>" data-url="<?=$url?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/play_btn_large.png" alt="<?=$arItem["NAME"]?>"></div>
 				<?if( $arItem["PROPERTIES"]["IS_NO_AUTH"]["VALUE_XML_ID"] != "Y" ){?>
 				<div class="only_subs">
 					<span>Доступно по подписке </span><img src="<?=SITE_TEMPLATE_PATH?>/images/only_subs_btn.png" alt="Доступно по подписке">
