b2f0b997a2207071ac45b4cda8b03af0393f032f
adoletskiy <adoletsky@indigos.ru>
2018-01-15 15:13:32 +0300

    articles in main - all link

--- bitrix/templates/slavmir/components/bitrix/news.list/index-articles/template.php
+++ bitrix/templates/slavmir/components/bitrix/news.list/index-articles/template.php
@@ -8,8 +8,8 @@
 			<div class="index_articles_item">
 				<div class="article_text">
 					<?/*?><p class="type">Славянская культура</p><?*/?>
-					<h3 class="art_name"><?=$arItem["NAME"]?></h3>
-					<article><?=$arItem["PREVIEW_TEXT"]?> <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="read">Читать</a></article>
+                    <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" style="text-decoration: none"><h3 class="art_name"><?=$arItem["NAME"]?></h3></a>
+					<article><a href="<?=$arItem["DETAIL_PAGE_URL"]?>" style="text-decoration: none;color: #000"><?=$arItem["PREVIEW_TEXT"]?></a> <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="read">Читать</a></article>
 				</div>
 				<div class="article_img">
 					<img src="<?=$image?>" alt="<?=$arItem["NAME"]?>">
