aa83129b6599c3246c301b9f56c60d9d19ec2b1d
adoletskiy <adoletsky@indigos.ru>
2018-02-13 08:11:32 +0300

    replace "[" & "]" in songs name

--- ajax/get-radio-info.php
+++ ajax/get-radio-info.php
@@ -14,6 +14,10 @@ $lastArr = array();
 $currArr = array();
 $i = 0;
 while($el = $rsData->fetch()){
+    //убрать из UF_NAME символы х [ и ]
+    $healthy = array("[", "]");
+    $yummy   = array("");
+    $el["UF_NAME"]=str_replace($healthy, $yummy, $el["UF_NAME"]);
 	if($el["UF_DBID"]){
 		if( $i == 0 ){
 			$currArr=array(
