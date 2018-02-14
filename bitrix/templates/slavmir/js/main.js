618ae5b3c5d1b3fc72cb400d5bc9c034148be5dc
adoletskiy <adoletsky@indigos.ru>
2018-02-14 14:37:51 +0300

    Stas update

--- bitrix/templates/slavmir/js/main.js
+++ bitrix/templates/slavmir/js/main.js
@@ -115,6 +115,7 @@ $(document).on('ready', function(){
 	  });*/
 
 	$('input[type="tel"]').mask("+7(999)999-99-99"); //phone mask
+	$('input[name="REGISTER[PERSONAL_MOBILE]"]').mask("+9(999)999-99-99"); //phone mask
 
 	$('.video_bar_slider').slick({
         infinite: true,
