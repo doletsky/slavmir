15a864aa73ee01d3603487d0650abbeaf62187ec
adoletskiy <adoletsky@indigos.ru>
2018-02-15 09:04:51 +0300

    class=subs in news slider

--- bitrix/templates/slavmir/components/bitrix/news.list/news-slider/template.php
+++ bitrix/templates/slavmir/components/bitrix/news.list/news-slider/template.php
@@ -18,7 +18,7 @@
 							?>
 							<div class="news_item">
 								<p class="item_date"><?=small_russian_date("j F",MakeTimeStamp($arNews["ACTIVE_FROM"],"DD.MM.YYYY"))?></p>
-								<a href="<?=$arNews["DETAIL_PAGE_URL"]?>">
+								<a href="<?=$arNews["DETAIL_PAGE_URL"]?>" <?if( $arNews["PROPERTY_IS_NO_AUTH_ENUM_ID"]!="49" ){?>class="subs"<?}?>>
 									<span class="name"><?=$arNews["NAME"]?> <?if( $arNews["PROPERTY_IS_NO_AUTH_ENUM_ID"]!="49" ){?><span class="subs_read_only"></span><?}?></span>
 									<div class="text"><?=$arNews["PREVIEW_TEXT"]?></div>
 								</a>
